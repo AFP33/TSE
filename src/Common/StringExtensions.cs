ead713d0bdcd51ea1437b98a5893c679b1b3b770
afp33 <afp33dev@gmail.com>
2023-03-03 16:55:47 +0330

    Bug fix (Persian number to English number)

--- src/Common/StringExtensions.cs
+++ src/Common/StringExtensions.cs
@@ -163,22 +163,29 @@ namespace Tse.Common
         /// <returns></returns>
         internal static string PersianToEnglish(this string persianStr)
         {
-            Dictionary<char, char> LettersDictionary = new Dictionary<char, char>
+            try
             {
-                ['۰'] = '0',
-                ['۱'] = '1',
-                ['۲'] = '2',
-                ['۳'] = '3',
-                ['۴'] = '4',
-                ['۵'] = '5',
-                ['۶'] = '6',
-                ['۷'] = '7',
-                ['۸'] = '8',
-                ['۹'] = '9'
-            };
-            foreach (var item in persianStr)
-                persianStr = persianStr.Replace(item, LettersDictionary[item]);
-            return persianStr;
+                Dictionary<char, char> LettersDictionary = new Dictionary<char, char>
+                {
+                    ['۰'] = '0',
+                    ['۱'] = '1',
+                    ['۲'] = '2',
+                    ['۳'] = '3',
+                    ['۴'] = '4',
+                    ['۵'] = '5',
+                    ['۶'] = '6',
+                    ['۷'] = '7',
+                    ['۸'] = '8',
+                    ['۹'] = '9'
+                };
+                foreach (var item in persianStr)
+                    persianStr = persianStr.Replace(item, LettersDictionary[item]);
+                return persianStr;
+            }
+            catch (Exception)
+            {
+                return persianStr;
+            }
         }
 
         /// <summary>
